9293ae46b810d5486afcf4e838641d0a11e75781
Renato Rosa Franco <renato_ny@live.com>
2019-07-18 16:38:08 -0300

    Level 3 - Component State

--- poweringupwithreact/src/components/CommentBox.js
+++ poweringupwithreact/src/components/CommentBox.js
@@ -3,14 +3,47 @@ import React from 'react';
 import Comment from './Comment';
 
 class CommentBox extends React.Component {
+	
+	constructor(props) {
+		super(props);
+
+		this.getComments = this.getComments.bind(this);
+		this.getCommentsTitle = this.getCommentsTitle.bind(this);
+	}
+
+	getCommentsTitle(commentCount) {
+		if(commentCount == 0) {
+			return 'No comments yet';
+		}else if (commentCount === 1) {
+			return '1 comment';
+		} else {
+			return `${commentCount} comments`;
+		}
+	}
+
+	getComments() {
+		const commentList = [
+			{id: 1, author: 'Morgan MrCircuit', body: 'Great Picture!'},
+			{id: 2, author: 'Bending Bender', body: 'Excellent stuff'},
+			{id: 3, author: 'Stevie Oznick', body: 'A gred Book'}
+		];
+
+		return commentList.map(comment => {
+			return <Comment key={comment.id} author={comment.author} 
+							body={comment.body} />
+		})
+	}
+
 	render(){
+		const comments = this.getComments();
+
 		return(
 			<div className='comment-box'>
 				<h3>Comments</h3>
-				<h4 className='comment-count'>2 comments</h4>
+				<h4 className='comment-count'>
+				{this.getCommentsTitle(comments.length)}</h4>
 				<div className='comment-list'>
-					<Comment author="Morgan MrCircuit" body="Great Picture!" />
-					<Comment author="Bending Bender" body="Excellent stuff" />
+					{comments}
 				</div>
 			</div>
 		);
